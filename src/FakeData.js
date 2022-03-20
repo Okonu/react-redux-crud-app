0a7c45e4560da2551b81b05cc7a4c369a46f0188
Okonu <ianyakundi015@gmail.com>
2022-03-20 20:21:32 +0300

    first commit

---
+++ src/FakeData.js
@@ -0,0 +1,42 @@
+export const UsersData = [
+  {
+    id: 1,
+    name: "Dohn Deborah",
+    username: "Memi",
+  },
+  {
+    id: 2,
+    name: "John Moha",
+    username: "Jomo",
+  },
+  {
+    id: 3,
+    name: "Win Chang",
+    username: "ChanWi",
+  },
+  {
+    id: 4,
+    name: "Qwerty Uipo",
+    username: "Puito",
+  },
+  {
+    id: 5,
+    name: "Beno Swet",
+    username: "Nova",
+  },
+  {
+    id: 6,
+    name: "Cheche Loper",
+    username: "Kuku",
+  },
+  {
+    id: 7,
+    name: "Daktari Waingo",
+    username: "Munio",
+  },
+  {
+    id: 8,
+    name: "Pullop Bumper",
+    username: "P_Bumper",
+  },
+];
